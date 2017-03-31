e0a9303bf95d68a6f65dabf97e81bcab8d9980cc
anilbhandary <anil20787@gmail.com>
2017-03-31 21:34:30 +0530

    Codechef problem BRLADDER

---
+++ codechef_brladder.rb
@@ -0,0 +1,27 @@
+def figure(a, b)
+  if a > b
+    temp = a
+    a = b
+    b = temp
+  end
+  
+  if (b-a) == 1 && a.odd?
+    return "YES"
+  elsif a.odd? && b.odd? && (b - a) == 2
+    return "YES"
+  elsif a.even? && b.even? && (b - a) == 2
+    return "YES"
+  else
+    return "NO"
+  end
+end
+
+
+q = gets.chomp.to_i
+r = []
+q.times do
+  a, b = gets.chomp.split(' ')
+  r << figure(a.to_i, b.to_i)
+end
+
+puts r
